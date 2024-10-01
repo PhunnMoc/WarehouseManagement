dde1b85a291ca43a6864b838ebb8885635a62628
BaoVu0712 <91738440+BaoVu0712@users.noreply.github.com>
2024-10-01 09:23:01 +0700

    fix Create all models and fake data (#4)

--- app/src/main/java/com/example/warehousemanagement/domain/model/packageDetail.kt
+++ app/src/main/java/com/example/warehousemanagement/domain/model/packageDetail.kt
@@ -3,10 +3,10 @@ package com.example.warehousemanagement.domain.model
 import androidx.core.location.LocationRequestCompat.Quality
 
 class packageDetail(
-    val idPackageDetail:String,
-    val idPackage:String,
-    val idProduct:String,
+    val idPackageDetail: String,
+    val idPackage: String,
+    val idProduct: String,
     val quality: Int,
-    val unitPrice:Double
+    val unitPrice: Double
 ) {
 }
