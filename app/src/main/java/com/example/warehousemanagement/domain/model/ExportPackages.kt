b0b77ab3d4ffb6b567d4e324daa4e4fee6b8335e
BaoVu0712 <91738440+BaoVu0712@users.noreply.github.com>
2024-10-02 22:04:59 +0700

    fix Create all models and fake data (#4)

--- app/src/main/java/com/example/warehousemanagement/domain/model/ExportPackages.kt
+++ app/src/main/java/com/example/warehousemanagement/domain/model/ExportPackages.kt
@@ -5,7 +5,7 @@ import java.util.Date
 data class ExportPackages(
     val idExportPackages: String,
     val packageName: String,
-
+    val listProduct: List<Product>,
     val totalAmount: Double,
     val exportDate: Date,
     val idCustomer: String,
--- app/src/main/java/com/example/warehousemanagement/domain/model/Product.kt
+++ app/src/main/java/com/example/warehousemanagement/domain/model/Product.kt
@@ -6,14 +6,14 @@ data class Product(
     val idProduct: String, // Định danh sản phẩm
     val productName: String, // Tên sản phẩm
     val genreId: String, // Loại sản phẩm
-    val stockQuantity: Int, // Số lượng sản phẩm
+    val quantity: Int, // Số lượng sản phẩm
     val description: String, // Mô tả sản phẩm
-    val importPrice: Double,
-    val sellingPrice: Double,
-    val supplierId: String, // Thông tin nhà cung cấp
+    val importPrice: Double = 0.0,
+    val sellingPrice: Double = 0.0,
+    val supplierId: String = "Unknown", // Thông tin nhà cung cấp
     val isInStock: Boolean, // Tình trạng hàng hóa
-    val barcode: String, // Mã vạch
-    val storageLocationId: String, // Khu vực
+    val barcode: String = "N/A", // Mã vạch
+    val storageLocationId: String = "Default", // Khu vực
     val lastUpdated: Date, // Ngày cập nhật cuối
-    val image: String
+    val image: String? = null
 )
