2cb6f2b246ba6aa7ab359d03ed68a2d255f0a944
Khanh Huynh <hongkhanh2901@gmail.com>
2024-10-09 19:13:18 +0700

    Ticket 13: Create the common UI

--- app/src/main/java/com/example/warehousemanagement/domain/feature/ui/common/ProductCard.kt
+++ app/src/main/java/com/example/warehousemanagement/domain/feature/ui/common/ProductCard.kt
@@ -1,32 +1,28 @@
 package com.example.warehousemanagement.domain.feature.ui.common
 
-import androidx.annotation.ColorRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.typography
 
 import androidx.compose.material3.Text
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.warehousemanagement.R
@@ -36,31 +32,90 @@ import java.util.Date
 @Composable
 fun ProductCard(product: Product) {
     Box(
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier
+            .padding(8.dp)
             .border(
                 width = 1.dp,
                 color = colorResource(id = R.color.line_light_gray),
-                shape = RoundedCornerShape(8.dp)
+                shape = RoundedCornerShape(10.dp)
             )
-            .background(colorResource(id = R.color.line_light_gray)),
+            .background(colorResource(id = R.color.icon_tint_white)),
 
-    ) {
+        ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+
         ) {
-            Text(text = product.productName, style = MaterialTheme.typography.headlineSmall)
+            Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(
+                        width = 1.dp,
+                        color = colorResource(id = R.color.line_light_gray),
+                        shape = RoundedCornerShape(10.dp)
+
+                    ).background(colorResource(id = R.color.background_gray)),
+
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.icons8_package), // Replace with your icon drawable resource
+                    contentDescription = "Product Icon"
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = product.productName,
+                    style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold),
+                    fontSize = 16.sp
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(text = product.idProduct,
+                    style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold),
+                    fontSize = 16.sp)
+                Spacer(modifier = Modifier.width(4.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.icons8_qr_32), // Replace with your QR code drawable resource
+                    contentDescription = "QR Code",
+
+
+                )
+            }
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = product.description, style = MaterialTheme.typography.bodyLarge)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                Text(text = product.genreId,
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontSize = 12.sp)
+
+
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Quantity: ${product.quantity}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontSize = 12.sp)
+
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Location: ${product.storageLocationId}",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontSize = 12.sp)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = if (product.isInStock) "Yes" else "No",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontSize = 12.sp,)
+            }
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Price: $${product.sellingPrice}", style = MaterialTheme.typography.bodyMedium)
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Quantity: ${product.quantity}", style = MaterialTheme.typography.bodyLarge)
+            Text(text = product.description,
+                style = MaterialTheme.typography.bodyMedium,
+                fontSize = 12.sp)
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
@@ -68,9 +123,9 @@ fun ProductCard(product: Product) {
 fun PreviewProductCard() {
     ProductCard(
         product = Product(
-            idProduct = "1",
-            productName = "Example Product",
-            genreId = "Genre",
+            idProduct = "QUAT_001",
+            productName = "Quat Senko",
+            genreId = "Digital Devices",
             quantity = 100,
             description = "This is an example product",
             importPrice = 50.0,
@@ -78,7 +133,7 @@ fun PreviewProductCard() {
             supplierId = "Supplier",
             isInStock = true,
             barcode = "1234567890",
-            storageLocationId = "Location",
+            storageLocationId = "A2",
             lastUpdated = Date(),
             image = null
         )
