092abb11476bc594d1695a6c770618acf7146e09
Khanh Huynh <hongkhanh2901@gmail.com>
2024-10-11 09:35:38 +0700

    Ticket 13: Update DialogView

--- app/src/main/java/com/example/warehousemanagement/domain/feature/ui/common/DialogView.kt
+++ app/src/main/java/com/example/warehousemanagement/domain/feature/ui/common/DialogView.kt
@@ -1,20 +1,15 @@
 package com.example.warehousemanagement.domain.feature.ui.common
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,8 +17,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.example.warehousemanagement.R
-
-// Composable function for a custom dialog view
 @Composable
 fun DialogView(
     title: String, // Title of the dialog
@@ -37,7 +30,7 @@ fun DialogView(
     Dialog(onDismissRequest = onCancel) {
         // Surface component for the dialog
         Surface(
-            shape = RoundedCornerShape(15.dp), // Rounded corners for the dialog
+            shape = RoundedCornerShape(25.dp), // Rounded corners for the dialog
             color = MaterialTheme.colorScheme.surface, // Color of the dialog surface
         ) {
             // Column for arranging the dialog components vertically
@@ -65,36 +58,36 @@ fun DialogView(
                     // Cancel button
                     Button(onClick = onCancel,
                         colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.background_gray)), // Gray background color for the cancel button
-                        shape = RoundedCornerShape(15.dp), // Rounded corners for the cancel button
+                        shape = RoundedCornerShape(50.dp), // Rounded corners for the cancel button
                         modifier = Modifier
                             .width(95.2.dp)  // Width of the cancel button
                             .height(38.6.dp) // Height of the cancel button
                     ) {
                         // Text for the cancel button
                         Text(text = cancelText,
-                            color = colorResource(id = R.color.icon_tint_black), // Black text color for the cancel button
-                            fontSize = 9.sp) // Font size for the cancel button
+                            color = colorResource(id = R.color.text_color_black), // Black text color for the cancel button
+                            fontSize = 9.sp,
+                            fontWeight = FontWeight.Normal)// Font size for the cancel button
                     }
                     // Confirm button
                     Button(onClick = onConfirm,
                         colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.background_theme)), // Theme background color for the confirm button
-                        shape = RoundedCornerShape(15.dp), // Rounded corners for the confirm button
+                        shape = RoundedCornerShape(50.dp), // Rounded corners for the confirm button
                         modifier = Modifier
                             .width(95.2.dp)  // Width of the confirm button
                             .height(38.6.dp) // Height of the confirm button
                     ) {
                         // Text for the confirm button
                         Text(text = confirmText,
-                            color = colorResource(id = R.color.icon_tint_black), // Black text color for the confirm button
-                            fontSize = 9.sp) // Font size for the confirm button
+                            color = colorResource(id = R.color.text_color_black), // Black text color for the confirm button
+                            fontSize = 9.sp,
+                            fontWeight = FontWeight.Normal) // Font size for the confirm button
                     }
                 }
             }
         }
     }
 }
-
-// Preview function for the DialogView
 @Preview(showBackground = true)
 @Composable
 fun PreviewDialogView() {
