edae528663321f2278f8e6f2b7750fc3472e5c08
Khanh Huynh <hongkhanh2901@gmail.com>
2024-10-13 21:05:43 +0700

    Ticket 13: Update MiniButton

--- app/src/main/java/com/example/warehousemanagement/domain/feature/ui/common/MiniButton.kt
+++ app/src/main/java/com/example/warehousemanagement/domain/feature/ui/common/MiniButton.kt
@@ -1,32 +1,32 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.warehousemanagement.R
 
 @Composable
-fun MiniButton(onClick: () -> Unit) {
+fun MiniButton(
+    onClick: () -> Unit,
+    icon: Painter,
+    contentDescription: String? = null
+) {
     IconButton(
         onClick = onClick,
         modifier = Modifier
-            .width(80.dp)  // Width of the button
+            .width(80.dp)
             .height(80.dp),
     ) {
         Image(
-
             modifier = Modifier.fillMaxSize(),
-
-            painter = painterResource(id = R.drawable.icons8_add_button_80),
-            contentDescription = "Add Icon"
+            painter = icon,
+            contentDescription = contentDescription
         )
     }
 }
@@ -34,5 +34,9 @@ fun MiniButton(onClick: () -> Unit) {
 @Preview(showBackground = true)
 @Composable
 fun PreviewMiniButton() {
-    MiniButton(onClick = { /*TODO: Handle button click*/ })
+    MiniButton(
+        onClick = { /* Handle button click */ },
+        icon = painterResource(id = R.drawable.icons8_add_button_80),
+        contentDescription = "Add Icon"
+    )
 }
